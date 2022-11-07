8c5b5ba0162dc972d4848dd1892e7b24547c2812
Maizer <qpalzmxdrfyved@gmail.com>
2022-11-07 18:19:13 -0500

    added rekognition api

--- lambda_functions/6998_hw2_LF1.py
+++ lambda_functions/6998_hw2_LF1.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -7,7 +8,25 @@ logger.setLevel(logging.DEBUG)
 def lambda_handler(event, context):
     # updated by CodePipeline
     logger.info(event)
+
+    # validate event
+    try:
+        bucket = event["Records"]["s3"]["bucket"]["name"]
+        photo = event["Records"]["s3"]["object"]["key"]
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
+
+    client = boto3.client("rekognition")
+    response = client.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': photo}},
+        MinConfidence=30,
+        MaxLabels=5) 
+    labels=response['CustomLabels']
+    logger.info(response)
+
     return {
         'statusCode': 200,
-        'body': event
+        'body': json.dumps(labels)
     }
